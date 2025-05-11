fde92e391fb025fda8fc0253cb131b2d02dd5f6b
Abrumz <made16jan03@gmail.com>
2025-05-11 16:25:26 +0700

    feat: add API and data interfaces for user, patrol, and reporting functionalities

---
+++ src/interfaces/data.ts
@@ -0,0 +1,209 @@
+import { PatroliResponse } from './api'
+
+export interface UserData {
+	id: number
+	accessId: number
+	name: string
+	email: string
+	registrationNumber: string
+	phoneNumber: string
+	organization: string
+	photo: string | null
+	role: number
+	roleLevel: number
+	roleName: string
+}
+
+export interface PenugasanData {
+	id: string
+	number: string
+}
+
+export interface UserGcData {
+	id: number
+	number: number
+}
+
+export interface NonPatroliUserData {
+	id: number
+	accessId: number
+	name: string
+	email: string
+	registrationNumber: string
+	role: number
+	organization: string
+}
+
+export interface KorwilData {
+	id: string
+	nama: string
+	kode: string
+	m_daops_id: string
+}
+
+export interface BalaiData {
+	id: string
+	code: string
+	name: string
+	region: string
+}
+
+export interface ProvinsiData {
+	kode_wilayah: string
+	nama_wilayah: string
+}
+
+export interface KabupatenData {
+	kode_wilayah: string
+	nama_wilayah: string
+}
+
+export interface SkNumberData<T = any> {
+	success: boolean
+	message: string | string[]
+	data?: T
+}
+
+export interface DaopsData {
+	id: string
+	code: string
+	name: string
+	balaiId: string
+}
+
+export interface KorwilDistinctData {
+	kode: string
+	nama: string
+}
+
+export interface DaopsList {
+	nama_daops: string
+}
+
+export interface RegionData {
+	id: string
+	code: string
+	name: string
+	type: string
+}
+
+export interface PatrolData {
+	 id: string;
+	latitude: string
+	longitude: string
+	marker: string
+	patroli: PatroliResponse | null
+}
+
+export interface PatrolResponse {
+  [x: string]: string
+  id_daerah_patroli: string
+  kategori_patroli: string
+  id_laporan_header: string
+  id: string; 
+}
+
+export interface SpotData {
+ latitude: string;
+	longitude: string;
+	marker: string;
+	patroli: PatroliData; 
+}
+
+export interface PatroliData { 
+	regu_patroli: {
+		daerah: {
+			nama_daops: string;
+		};
+		ketua: {
+			nama: string;
+			email: string;
+		};
+	};
+	laporanDarat: Array<{
+		latitude: string;
+		longitude: string;
+	}>; 
+	id_laporan_header: string;
+	kategori_patroli: string;
+	id_daerah_patroli: {
+		nama_daerah_patroli: string;
+		tipe_wilayah: string;
+		nama_wilayah: string;
+	};
+	status_padam?: string;
+	my_daops?: boolean;
+}
+
+export interface PatrolListData {
+	reportLink: string
+	patrolRegion: string
+	operationRegion: string
+	patrolDate: string
+}
+
+export interface SuratTugasData {
+	id: string
+	number: string
+	type: string
+	startDate: string
+	finishDate: string
+	reportLink: string
+}
+
+export interface UserGroundcheckData {
+	id: string
+	nama: string
+	email: string
+	provinsi: string
+	kabupaten: string
+	patroli: string
+	daops: string
+	tanggal: string
+	anggota: string
+}
+
+export interface SuratTugasTeamMemberData {
+	id: string
+	name: string
+	registrationNumber: string
+	organization: string
+	startDate: string
+	endDate: string
+	posko: string
+	daops: string
+}
+
+export interface SuratTugasLaporanData {
+	id_laporan_header: string
+	tanggal_patroli: string
+	nama_daerah_patroli: string
+	nama_daops: string
+	nama_ketua: string
+}
+
+export interface UserGroundCheckData {
+	id: string
+}
+export interface LaporanData {
+	id: string
+	id_daerah_patroli: string
+	kategori_patroli: string
+	tanggal_patroli: string
+}
+
+export interface PoskoData {
+	id: string
+	name: string
+	daops: string
+	daopsId: string
+	kecamatan: string
+	kecamatanId: string
+}
+
+export interface RoleData {
+	id: number
+	name: string
+	level: number
+	active: string
+}
---
+++ src/interfaces/internal.ts
@@ -0,0 +1,149 @@
+import {
+	LaporanData,
+	NonPatroliUserData,
+	PatrolData,
+	PatrolListData,
+	PenugasanData,
+	UserData,
+	SuratTugasLaporanData
+} from './data'
+
+export interface ValidatorResult {
+	pass: boolean
+	message: string
+}
+
+export interface ServiceResponse<T = any> {
+	success: boolean
+	message: string | string[]
+	data?: T
+}
+
+export interface AuthValidatorResult extends ValidatorResult {
+	emailError: boolean
+	passwordError: boolean
+}
+
+interface PatrolCounter {
+	mandiri: number
+	rutin: number
+	terpadu: number
+	padam: number
+}
+
+export interface PatroliServiceResponse {
+	patroliSpots: PatrolData[]
+	counter: PatrolCounter
+	patroliTerpadu: PatrolListData[]
+	patroliMandiri: PatrolListData[]
+	patroliRutin: PatrolListData[]
+	pemadaman: PatrolListData[]
+}
+
+export interface LoginInput {
+	email: string
+	password: string
+}
+
+export interface ResetInput {
+	password: string
+	retype_password: string
+}
+
+export interface SendInput {
+	email: string
+}
+
+export interface AddPoskoInput {
+	name: string
+	daops: string
+	kecamatan: string
+}
+export interface UpdatePoskoInput extends AddPoskoInput {
+	id: string
+}
+
+interface BaseUserInput {
+	name: string
+	email: string
+	registrationNumber: string
+	phoneNumber: string
+	password?: string
+	cPassword?: string
+	r_role_id: string
+}
+
+interface GcUserInput {
+	name: string
+	email: string
+	password?: string
+	cPassword?: string
+	provinsi: string
+	kabupaten: string
+	patroli: string
+	daops: string
+	startDate: string
+	anggota: string
+}
+
+export interface updateGcUserInput {
+	id: string
+	name: string
+	email: string
+	password?: string
+	cPassword?: string
+	provinsi: string
+	kabupaten: string
+	patroli: string
+	daops: string
+	startDate: string
+	anggota: string
+}
+
+export type AddUserInput = BaseUserInput
+
+export type AddUserGcInput = GcUserInput
+
+export interface UpdateUserInput extends BaseUserInput {
+	id: string
+	oldEmail: string
+	oldRegistrationNumber: string
+}
+
+export type DeleteUserInput = UserData
+
+export type DeletePenugasanInput = PenugasanData
+
+export type DeleteLaporanInput = SuratTugasLaporanData
+
+export interface AddPatroliNonLoginUserInput {
+	name: string
+	role: number
+}
+
+export interface UpdatePatroliNonLoginUserInput
+	extends AddPatroliNonLoginUserInput {
+	id: number
+}
+
+export type DeletePatroliNonLoginUserInput = UpdatePatroliNonLoginUserInput
+
+export interface CreateNonPatroliUser {
+	id: string
+	name: string
+	email: string
+	role: string
+	organization: string
+}
+
+export interface SendEmailForgot {
+	email: string
+}
+
+export interface resetPassword {
+	signature: string
+	password: string
+	retype_password: string
+}
+
+export type DeleteNonPatroliUser = NonPatroliUserData
