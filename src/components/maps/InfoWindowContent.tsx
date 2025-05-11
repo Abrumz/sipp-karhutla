58b7618deb584fa246035404fc7d02ab090e3e12
Abrumz <made16jan03@gmail.com>
2025-05-11 20:01:29 +0700

    refactor: remove close button from InfoWindowContent component

--- src/components/maps/InfoWindowContent.tsx
+++ src/components/maps/InfoWindowContent.tsx
@@ -78,26 +78,6 @@ const InfoWindowContent: React.FC<InfoWindowContentProps> = ({ patroli, isLogged
                     </div>
                 </div>
 
-                {/* Close button with red circle */}
-                <div
-                    onClick={onClose}
-                    className="close-button w-6 h-6 rounded-full bg-white flex items-center justify-center cursor-pointer ml-2 border-2 border-red-500 box-border flex-shrink-0"
-                >
-                    <svg
-                        width="14"
-                        height="14"
-                        viewBox="0 0 14 14"
-                        fill="none"
-                        xmlns="http://www.w3.org/2000/svg"
-                    >
-                        <path
-                            d="M1 1L13 13M1 13L13 1"
-                            stroke="#333"
-                            strokeWidth="2"
-                            strokeLinecap="round"
-                        />
-                    </svg>
-                </div>
             </div>
 
             <div className="p-3 bg-white">
