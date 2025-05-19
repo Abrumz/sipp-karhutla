3552f604d2f0cd285c78a2f6c507b6efe07a4ee9
Abrumz <made16jan03@gmail.com>
2025-05-19 23:08:45 +0700

    feat: add PelaporanRentangTanggal component and page for date range report visualization and download functionality

---
+++ src/pages/pelaporan/rentang-tanggal/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import SiteLayout from '@/components/layout/siteLayout/SiteLayout';
+import PelaporanRentangTanggal from '@/components/pelaporan/rentangTanggal/RentangTanggal';
+import CustomHead from '@/components/layout/customHead/CustomHead';
+
+export default function PelaporanRentangTanggalPage() {
+    return (
+        <SiteLayout >
+            <CustomHead
+                title='Rentang Tanggal'
+                description='Platform visualisasi data monitoring patroli dan pemadaman kebakaran hutan dan lahan'
+            >
+                <div>
+                    <PelaporanRentangTanggal />
+                </div>
+            </CustomHead>
+        </SiteLayout>
+    );
+}
