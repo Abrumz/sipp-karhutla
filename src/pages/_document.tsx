205d8c39cc5feaafb6658f8e6572e7d920f10b5e
Abrumz <made16jan03@gmail.com>
2025-05-25 00:48:39 +0700

    feat: add Leaflet CSS link for improved map functionality

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,8 +8,14 @@ export default function Document() {
                 <meta charSet="utf-8" />
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
 
-                {/* Favicon dengan atribut tambahan */}
                 <link rel="shortcut icon" href="/favicon.ico" />
+
+                <link
+                    rel="stylesheet"
+                    href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css"
+                    integrity="sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A=="
+                    crossOrigin=""
+                />
             </Head>
             <body>
                 <Main />
